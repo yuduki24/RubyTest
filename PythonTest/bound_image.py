c0d3de6b381185d28eca325ae8ddb74a99fd1918
yuduki <rtccb.yuduki24@gmail.com>
2017-11-03 21:22:45 +0900

    [add]画像移動と跳ね返りプログラムの追加.

---
+++ PythonTest/bound_image.py
@@ -0,0 +1,37 @@
+import pygame
+from pygame.locals import *
+import sys
+ 
+SCR_WIDTH,SCR_HEIGHT = 640,480
+ 
+pygame.init()
+screen = pygame.display.set_mode((SCR_WIDTH,SCR_HEIGHT))
+pygame.display.set_caption(u"画像の移動と跳ね返り処理")
+ 
+img = pygame.image.load("img/my_python.png").convert()
+colorkey = img.get_at((0, 0))
+img.set_colorkey(colorkey, RLEACCEL)
+img_rect = img.get_rect()
+ 
+vx = vy = 500  # 1秒間の移動ピクセル
+clock = pygame.time.Clock()
+ 
+while True:
+    time_passed = clock.tick(60)  # 60fpsで前回からの経過時間を返す（ミリ秒）
+    time_passed_seconds = time_passed / 1000.0  # ミリ秒を秒に変換
+    
+    # 画像の移動
+    img_rect.x += vx * time_passed_seconds
+    img_rect.y += vy * time_passed_seconds
+    # 跳ね返り処理
+    if img_rect.left < 0 or img_rect.right > SCR_WIDTH:
+        vx = -vx
+    if img_rect.top < 0 or img_rect.bottom > SCR_HEIGHT:
+        vy = -vy
+    
+    screen.fill((0,0,255))
+    screen.blit(img, img_rect)
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT: sys.exit()
+        if event.type == KEYDOWN and event.key == K_ESCAPE: sys.exit()
